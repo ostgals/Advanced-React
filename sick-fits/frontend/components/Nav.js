0b6a680f84d2235fab938e3e73caa86b1139a396
Sunset Sarsaparilla <ostgals@gmail.com>
2018-12-16 22:31:30 +0200

    module 2 is over

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import Link from 'next/link';
 
+import NavStyles from './styles/NavStyles';
+
 export default () => (
-  <div>
-    <Link href="/sell">
-      <a>Sell!</a>
-    </Link>
+  <NavStyles>
     <Link href="/">
-      <a>Home!</a>
+      <a>Items</a>
     </Link>
-  </div>
+    <Link href="/sell">
+      <a>Sell</a>
+    </Link>
+    <Link href="/signup">
+      <a>Signup</a>
+    </Link>
+    <Link href="/orders">
+      <a>Orders</a>
+    </Link>
+    <Link href="/me">
+      <a>Account</a>
+    </Link>
+  </NavStyles>
 );
--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,28 +1,67 @@
 import React, { Component } from 'react';
-import styled from 'styled-components';
+import styled, { ThemeProvider, injectGlobal } from 'styled-components';
 
 import Meta from './Meta';
 import Header from './Header';
 
+const theme = {
+  red: '#FF0000',
+  black: '#393939',
+  grey: '#3A3A3A',
+  lightgrey: '#E1E1E1',
+  offWhite: '#EDEDED',
+  maxWidth: '1000px',
+  bs: '0 12px 24px 0 rgba(0, 0, 0, 0.09)',
+};
+
 const StyledPage = styled.div`
   background: white;
-  color: black;
+  color: ${props => props.theme.black};
 `;
 
 const Inner = styled.div`
-  max-width: 1000px;
+  max-width: ${props => props.theme.maxWidth};
   margin: 0 auto;
   padding: 2rem;
+`;
+
+injectGlobal`
+  @font-face {
+    font-family: 'radnika_next';
+    src: url('/static/radnikanext-medium-webfont.woff2');
+    font-weight: normal;
+    font-style: normal;
+  }
+  html {
+    font-size: 10px;
+    box-sizing: border-box;
+  }
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+  body {
+    padding: 0;
+    margin: 0;
+    font-size: 1.5rem;
+    font-family: 'radnika_next';
+    line-height: 2;
+  }
+  a {
+    text-decoration: none;
+    color: ${theme.black};
+  }
 `;
 
 class Page extends Component {
   render() {
     return (
-      <StyledPage>
-        <Meta />
-        <Header />
-        <Inner>{this.props.children}</Inner>
-      </StyledPage>
+      <ThemeProvider theme={theme}>
+        <StyledPage>
+          <Meta />
+          <Header />
+          <Inner>{this.props.children}</Inner>
+        </StyledPage>
+      </ThemeProvider>
     );
   }
 }
