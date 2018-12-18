aaf81b935449f119b88b864eb31d5320ad83c2b4
Sunset Sarsaparilla <ostgals@gmail.com>
2018-12-18 22:06:21 +0200

    CreateItem form added

--- sick-fits/frontend/components/CreateItem.js
+++ sick-fits/frontend/components/CreateItem.js
@@ -3,6 +3,15 @@ import { Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
 
 import Form from './styles/Form';
+import ErrorMessage from './ErrorMessage';
+
+const CREATE_ITEM_MUTATION = gql`
+  mutation CREATE_ITEM_MUTATION($data: ItemCreateInput!) {
+    createItem(data: $data) {
+      id
+    }
+  }
+`;
 
 class CreateItem extends Component {
   state = {
@@ -19,48 +28,59 @@ class CreateItem extends Component {
   };
   render() {
     return (
-      <Form onSubmit={e => e.preventDefault()}>
-        <fieldset>
-          <label htmlFor="title">
-            Title
-            <input
-              type="text"
-              name="title"
-              id="title"
-              required
-              placeholder="Title"
-              value={this.state.title}
-              onChange={this.handleChange}
-            />
-          </label>
+      <Mutation mutation={CREATE_ITEM_MUTATION} variables={this.state}>
+        {(createItem, { loading, error }) => (
+          <Form
+            onSubmit={e => {
+              e.preventDefault();
+              console.log(this.state);
+            }}
+          >
+            <ErrorMessage error={error} />
+            <fieldset disabled={loading}>
+              <label htmlFor="title">
+                Title
+                <input
+                  type="text"
+                  name="title"
+                  id="title"
+                  required
+                  placeholder="Title"
+                  value={this.state.title}
+                  onChange={this.handleChange}
+                />
+              </label>
 
-          <label htmlFor="price">
-            Price
-            <input
-              type="number"
-              name="price"
-              id="price"
-              required
-              placeholder="Price in cents"
-              value={this.state.price}
-              onChange={this.handleChange}
-            />
-          </label>
+              <label htmlFor="price">
+                Price
+                <input
+                  type="number"
+                  name="price"
+                  id="price"
+                  required
+                  placeholder="Price in cents"
+                  value={this.state.price}
+                  onChange={this.handleChange}
+                />
+              </label>
 
-          <label htmlFor="description">
-            Description
-            <textarea
-              name="description"
-              id="description"
-              placeholder="Enter a desctiption"
-              value={this.state.description}
-              onChange={this.handleChange}
-            />
-          </label>
+              <label htmlFor="description">
+                Description
+                <textarea
+                  name="description"
+                  id="description"
+                  required
+                  placeholder="Enter a desctiption"
+                  value={this.state.description}
+                  onChange={this.handleChange}
+                />
+              </label>
 
-          <button type="submit">Submit</button>
-        </fieldset>
-      </Form>
+              <button type="submit">Submit</button>
+            </fieldset>
+          </Form>
+        )}
+      </Mutation>
     );
   }
 }
