219b7c4c5e798024ce30ecd92ccd5569310244b3
Andrejs Kuzmins <ostgals@gmail.com>
2018-12-20 16:10:49 +0200

    reset password flow complete (backend)

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const { randomBytes } = require('crypto');
 
 const Mutations = {
   createItem(parent, args, ctx, info) {
@@ -66,6 +67,61 @@ const Mutations = {
   signout(parent, args, ctx, info) {
     ctx.response.clearCookie('token');
     return { message: 'Good Bye!' };
+  },
+
+  async requestReset(parent, { email }, ctx, info) {
+    const user = await ctx.db.query.user({ where: { email } });
+    if (!user) {
+      throw new Error(`No user found for email ${email}!`);
+    }
+
+    const resetToken = randomBytes(20).toString('hex');
+    const resetTokenExpiry = Date.now() + 3600000;
+
+    await ctx.db.mutation.updateUser({
+      where: { email },
+      data: { resetToken, resetTokenExpiry },
+    });
+
+    return {
+      message: `Reset password link is sent to ${email}!`,
+    };
+  },
+
+  async resetPassword(parent, args, ctx, info) {
+    const { resetToken, password, confirmPassword } = args;
+    // 1. check if passwords match
+    if (password !== confirmPassword) {
+      throw new Error('Passwords do not match!');
+    }
+    // 2. check if token is legit
+    const [user] = await ctx.db.query.users({
+      where: {
+        resetToken,
+        resetTokenExpiry_gte: Date.now() - 3600000,
+      },
+      first: 1,
+    });
+    if (!user) {
+      throw new Error('This reset token is either invalid or expired!');
+    }
+    // 3. update password and delete resetToken
+    const updatedUser = await ctx.db.mutation.updateUser({
+      where: { id: user.id },
+      data: {
+        password: await bcrypt.hash(password, 10),
+        resetToken: null,
+        resetTokenExpiry: null,
+      },
+    });
+    // 4. create and save JWT
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+    ctx.response.cookie('token', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year
+    });
+    // 5. return updatedUser
+    return updatedUser;
   },
 };
 
--- sick-fits/frontend/pages/signup.js
+++ sick-fits/frontend/pages/signup.js
@@ -14,7 +14,6 @@ export default () => (
     <Columns>
       <Signup />
       <Signin />
-      <Signup />
     </Columns>
   </div>
 );
